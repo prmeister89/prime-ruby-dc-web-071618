8fc11cdf3528e83d648e568642b5f9514107452f
Peter Meister <prmeister89@gmail.com>
2018-07-06 15:13:58 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -4,6 +4,10 @@ def prime?(x)
   if x/x == 1 && x/1 == x
     a.push(x)
     return true
+  elsif x<0
+    -(x)/x==1 && -(x)/1 == -(x)
+    a.push(x)
+    return true
   else
     return false
   end
