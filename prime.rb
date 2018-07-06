e96aa64e08ffc803c31a39fa096059e9e4669ba3
Peter Meister <prmeister89@gmail.com>
2018-07-06 16:10:34 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,9 @@
 def prime?(x)
   a = (2..x).to_a
-  prime_array = []
-  prime_test = x/x == 1 && x/1 == x
-  
-  
-  
+
   a.each do |element|
-    prime_array << element
-  
+    x%element == 
+    
   
   if prime_test
     prime_array.push(x)
