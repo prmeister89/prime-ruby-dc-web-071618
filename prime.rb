a6fb407c8450b754ee3b38d13a6c6f0729604c68
Peter Meister <prmeister89@gmail.com>
2018-07-06 16:22:56 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,11 @@
 def prime?(x)
   a = (2..x).to_a
 
-  a.all? do |element|
-    x%element != 0
+  if x<=0 || x==1
+    return false
+  else
+    a.all? do |element|
+      x%element != 0
   end
 end
     
