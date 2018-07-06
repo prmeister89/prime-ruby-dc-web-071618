c71ac8aa65771e3c9b27700f6a423e0fc19e93d3
Peter Meister <prmeister89@gmail.com>
2018-07-06 16:27:20 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(x)
+  a = (2..x-1).to_a
+
+  if x<=0 || x==1
+    return false
+  else
+    a.all? do |element|
+      x%element != 0
+    end
+  end
+end
+    
+    
+  
+
